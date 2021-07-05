396ec055c52faf0f2d84b44876a3a9699c3e4d3e
Balvinder471 <64981874+Balvinder471@users.noreply.github.com>
2021-07-05 15:07:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import cv2
-#from  PIL import Image, ImageOps
+# -*- coding: utf-8 -*-
+
 import streamlit as st 
 from PIL import Image
 import pickle
@@ -14,11 +14,11 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 #from keras.models import load_model
 
 html_temp = """
-   <div class="" style="background-color:blue;" >
+   <div class="" style="background-color:orange;" >
    <div class="clearfix">           
    <div class="col-md-12">
    <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Digital Image Processing lab</p></center> 
+   <center><p style="font-size:30px;color:white;margin-top:10px;">Mid Term 2 Practical</p></center> 
    </div>
    </div>
    </div>
@@ -26,22 +26,25 @@ html_temp = """
 st.markdown(html_temp,unsafe_allow_html=True)
   
 st.title("""
-        Collor Palette
+        Image Transformation
          """
          )
 file= st.file_uploader("Please upload image", type=("jpg", "png"))
-R = st.slider('R', min_value=0, max_value=255, step=1)
-G = st.slider('G', min_value=0, max_value=255, step=1)
-B = st.slider('B', min_value=0, max_value=255, step=1)
+kind = st.selectbox("Reflect-X: ",
+                     ['Reflect-X', 'Reflect-Y', 'Translation', 'Rotation', 'Shearing', 'Cropping'])
 
-
-def import_and_predict(image_data):
+import cv2
+from  PIL import Image, ImageOps
+def apply_transform(image_data):
   #img = image.load_img(image_data, target_size=(224, 224))
   #image = image.img_to_array(img)
   #img_reshap= np.expand_dims(image, axis=0)
   #img_reshap = preprocess_input(img_reshap)
    
-  image_data[:] = [R,G,B]
+  if kind == "Reflect-X":
+     image_data = cv.flip(image_data, 0)
+  elif kind == "Reflect-Y":
+     image_data = cv.flip(image_data, 1)
   st.image(image_data, use_column_width=True)
   return 0
 if file is None:
@@ -51,17 +54,17 @@ else:
   image = cv2.imdecode(file_bytes, 1)
   st.image(file,caption='Uploaded Image.', use_column_width=True)
     
-if st.button("Change Color"):
-  result=import_and_predict(image)
+if st.button("Apply Transformation"):
+  result=apply_transform(image)
   
 if st.button("About"):
-  st.header(" Bhavesh Kumawat")
-  st.subheader("Student, Department of Computer Engineering,PIET")
+  st.header("Balvinder Singh")
+  st.subheader("Student, Department of Computer Engineering 6th Semester")
 html_temp = """
    <div class="" style="background-color:orange;" >
    <div class="clearfix">           
    <div class="col-md-12">
-   <center><p style="font-size:20px;color:white;margin-top:10px;">Digital Image processing Experiment</p></center> 
+   <center><p style="font-size:20px;color:white;margin-top:10px;">DIP Midterm 2</p></center> 
    </div>
    </div>
    </div>
